522cd04e9f7ee4306efcebcc2d019d329582d78b
Walisson <Walissonoliveira1@outlook.com>
2026-05-11 23:37:32 -0300

    Orientação ao objeto usando pyhton

---
+++ Produto.py
@@ -0,0 +1,21 @@
+class Produto:
+	
+	def __init__(self):
+		
+		self.nome = ''
+		self.marca =''
+		self.model = ''
+		self.valor = ''
+
+
+produto = Produto('Celular', 2000,'Samsung', 'J8')
+
+produto1 = Produto('Geladeira',4000, 'Brastemo', 'BST900')
+
+
+print(produto0.__dict__)
+print(produto1.__dict__)
+print(produto2.__dict__)
+
+
+
