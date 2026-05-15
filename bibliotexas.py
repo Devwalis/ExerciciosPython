f9414dfccadaa99e84773f8247408dd69a6f1dee
Walisson <Walissonoliveira1@outlook.com>
2026-05-15 07:51:59 -0300

    Usando bibliotecas internas do python

---
+++ bibliotexas.py
@@ -0,0 +1,24 @@
+import math
+import random
+
+'''
+nota = 6.8
+print(round(nota, 2))
+
+
+print(math.floor(nota))
+
+print(math.ceil(nota))
+
+print(math.
+'''
+
+print(round(random.random()* 100))
+
+print(random.randint(1, 20)
+
+
+
+
+
+
