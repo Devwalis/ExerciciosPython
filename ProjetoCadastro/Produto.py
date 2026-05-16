8802397fa3faa731393e2d02005f2cd1af71103e
Walisson <Walissonoliveira1@outlook.com>
2026-05-16 09:58:34 -0300

    Começando um projeto para cadastar produtos usando conceitos de POO

---
+++ ProjetoCadastro/Produto.py
@@ -0,0 +1,7 @@
+class Produto:
+	def __init__(self, codigo, nome, quantidade, valor_unitario):
+		self.codigo = codigo
+		self.nome = nome
+		self.quantidade = quantidade
+		self.valor_unitario = valor_unitario
+		
