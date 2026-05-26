d530dd17f8ce48a60bc77e2c8420f506d9dc0758
Walisson <Walissonoliveira1@outlook.com>
2026-05-26 20:14:13 -0300

    aprimorando o menu do projeto de crud de produtos

--- ProjetoCadastro/db/index.py
+++ ProjetoCadastro/db/index.py
@@ -15,3 +15,20 @@ opcao = 1
 while opcao != 0:
 	menu()
 	opcao = int(input('Escolha uma opção: '))
+	
+	
+	match opcao:
+		case 1:
+			print('****************************'
+			Produto.listarTodos()
+			print('****************************'
+			
+		case 2:
+			Produto.inserir()
+
+
+		case 3:
+			Produto.alterar()
+
+		case 4:
+			Produto.excluir()
