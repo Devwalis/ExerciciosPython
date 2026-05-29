24227d96a6b68a106921c23472f54923cf83d93a
Walisson <Walissonoliveira1@outlook.com>
2026-05-29 20:10:09 -0300

    api de conversão de moedas e tratamentos de erros

--- ProjetoConversao/conversao.py
+++ ProjetoConversao/conversao.py
@@ -8,7 +8,18 @@ def get_cotacao():
 	
 	response = requests.get(url)
 	data = response.json
+		
+	if response.status_code == 200:
+		
+		return data["rates"]
+	else:
+		print("erro ao obter cotações: ", response.status_code)
+		return None
+	
+	
+rates = get_cotacao()
+print("USD:", 1 / rates['USD'])
+print("EUR:", 1 / rates['EUR'])
+print("GBP:", 1 / rates['GBP'])
 
-	print(data)
-get_cotacao()
 
