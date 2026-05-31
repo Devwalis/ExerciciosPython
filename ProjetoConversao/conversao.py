d17205019d4c0922733db61d784d0cda9532c35d
Walisson <Walissonoliveira1@outlook.com>
2026-05-31 00:48:37 -0300

    Comparando cotação de moeda uma com a outra

--- ProjetoConversao/conversao.py
+++ ProjetoConversao/conversao.py
@@ -21,7 +21,41 @@ def converter_cotacao(origem = 'USD', destino = 'BRL', valor= 1):
 	return round(1 / rates['origem'], 4)
 
 
-print(converter_cotacao('USD', 'BRL'))
+
+def menu():
+	print()
+	print('1 - Converter Dolar em Real')
+	print('2 - Converter Euro em Real')
+	print('3 - Converter Libras em Real')
+	print('4 - Outra cotação')
+	print()
 
 
 
+
+opcap = 1
+
+
+while opcao != 0:
+	menu()
+	opcao = int(input("Escolha uma opção: "))	
+	
+	destino = 'BRL'
+	valor = input("Digite o valor: ")
+
+	match opcao: 
+		case 1: origem = 'USD'
+
+		case 2: origem = 'EUR'
+
+		case 3: origem = 'GBP'		
+
+		case 4: 
+			origem = input("Digite a Origem: " )
+			destino = input("Digite o Destino: ")
+	
+	print()
+	print('***************************************')
+	print(f'{origem} para {destino}: ', converter_cotacao(origem))
+	print('***************************************')
+	print()	
